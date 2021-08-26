4675ffc363ccf997969380facf04c5f55379c81a
Lucas <lupena@unal.edu.co>
2021-08-26 20:29:21 +0000

    4k

--- script_ejecutar_todo.sh
+++ script_ejecutar_todo.sh
@@ -1,73 +1,148 @@
+#!/bin/bash
 
-mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out.jpg 5
-mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out.jpg 5
-mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out.jpg 5
-mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out.jpg 5
-mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out.jpg 5
 
-mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out.jpg 6
-mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out.jpg 6
-mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out.jpg 6
-mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out.jpg 6
-mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out.jpg 6
+mpirun -np 1 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_1.jpg 1
+mpirun -np 1 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_1.jpg 1
+mpirun -np 1 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_1.jpg 1
+mpirun -np 1 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_1.jpg 1
+mpirun -np 1 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_1.jpg 1
 
-mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out.jpg 7
-mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out.jpg 7
-mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out.jpg 7
-mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out.jpg 7
-mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out.jpg 7
+mpirun -np 2 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_2.jpg 2
+mpirun -np 2 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_2.jpg 2
+mpirun -np 2 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_2.jpg 2
+mpirun -np 2 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_2.jpg 2
+mpirun -np 2 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_2.jpg 2
 
-mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out.jpg 8
-mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out.jpg 8
-mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out.jpg 8
-mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out.jpg 8
-mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out.jpg 8
+mpirun -np 3 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_3.jpg 3
+mpirun -np 3 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_3.jpg 3
+mpirun -np 3 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_3.jpg 3
+mpirun -np 3 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_3.jpg 3
+mpirun -np 3 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_3.jpg 3
 
-mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out.jpg 5
-mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out.jpg 5
-mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out.jpg 5
-mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out.jpg 5
-mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out.jpg 5
+mpirun -np 4 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_4.jpg 4
+mpirun -np 4 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_4.jpg 4
+mpirun -np 4 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_4.jpg 4
+mpirun -np 4 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_4.jpg 4
+mpirun -np 4 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_4.jpg 4
 
-mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out.jpg 6
-mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out.jpg 6
-mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out.jpg 6
-mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out.jpg 6
-mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out.jpg 6
+mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_5.jpg 5
+mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_5.jpg 5
+mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_5.jpg 5
+mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_5.jpg 5
+mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_5.jpg 5
 
-mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out.jpg 7
-mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out.jpg 7
-mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out.jpg 7
-mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out.jpg 7
-mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out.jpg 7
+mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_6.jpg 6
+mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_6.jpg 6
+mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_6.jpg 6
+mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_6.jpg 6
+mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_6.jpg 6
 
-mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out.jpg 8
-mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out.jpg 8
-mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out.jpg 8
-mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out.jpg 8
-mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out.jpg 8
+mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_7.jpg 7
+mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_7.jpg 7
+mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_7.jpg 7
+mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_7.jpg 7
+mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_7.jpg 7
 
-mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout.jpg 5
-mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout.jpg 5
-mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout.jpg 5
-mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout.jpg 5
-mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout.jpg 5
+mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_8.jpg 8
+mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_8.jpg 8
+mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_8.jpg 8
+mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_8.jpg 8
+mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 720bg.jpg 720fg.jpg 720out_8.jpg 8
 
-mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout.jpg 6
-mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout.jpg 6
-mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout.jpg 6
-mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout.jpg 6
-mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout.jpg 6
+mpirun -np 1 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_1.jpg 1
+mpirun -np 1 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_1.jpg 1
+mpirun -np 1 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_1.jpg 1
+mpirun -np 1 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_1.jpg 1
+mpirun -np 1 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_1.jpg 1
 
-mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout.jpg 7
-mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout.jpg 7
-mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout.jpg 7
-mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout.jpg 7
-mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout.jpg 7
+mpirun -np 2 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_2.jpg 2
+mpirun -np 2 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_2.jpg 2
+mpirun -np 2 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_2.jpg 2
+mpirun -np 2 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_2.jpg 2
+mpirun -np 2 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_2.jpg 2
 
-mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout.jpg 8
-mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout.jpg 8
-mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout.jpg 8
-mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout.jpg 8
-mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout.jpg 8
+mpirun -np 3 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_3.jpg 3
+mpirun -np 3 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_3.jpg 3
+mpirun -np 3 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_3.jpg 3
+mpirun -np 3 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_3.jpg 3
+mpirun -np 3 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_3.jpg 3
+
+mpirun -np 4 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_4.jpg 4
+mpirun -np 4 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_4.jpg 4
+mpirun -np 4 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_4.jpg 4
+mpirun -np 4 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_4.jpg 4
+mpirun -np 4 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_4.jpg 4
+
+mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_5.jpg 5
+mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_5.jpg 5
+mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_5.jpg 5
+mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_5.jpg 5
+mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_5.jpg 5
+
+mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_6.jpg 6
+mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_6.jpg 6
+mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_6.jpg 6
+mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_6.jpg 6
+mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_6.jpg 6
+
+mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_7.jpg 7
+mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_7.jpg 7
+mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_7.jpg 7
+mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_7.jpg 7
+mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_7.jpg 7
+
+mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_8.jpg 8
+mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_8.jpg 8
+mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_8.jpg 8
+mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_8.jpg 8
+mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 1080bg.jpg 1080fg.jpg 1080out_8.jpg 8
+
+mpirun -np 1 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_1.jpg 1
+mpirun -np 1 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_1.jpg 1
+mpirun -np 1 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_1.jpg 1
+mpirun -np 1 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_1.jpg 1
+mpirun -np 1 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_1.jpg 1
+
+mpirun -np 2 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_2.jpg 2
+mpirun -np 2 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_2.jpg 2
+mpirun -np 2 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_2.jpg 2
+mpirun -np 2 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_2.jpg 2
+mpirun -np 2 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_2.jpg 2
+
+mpirun -np 3 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_3.jpg 3
+mpirun -np 3 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_3.jpg 3
+mpirun -np 3 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_3.jpg 3
+mpirun -np 3 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_3.jpg 3
+mpirun -np 3 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_3.jpg 3
+
+mpirun -np 4 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_4.jpg 4
+mpirun -np 4 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_4.jpg 4
+mpirun -np 4 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_4.jpg 4
+mpirun -np 4 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_4.jpg 4
+mpirun -np 4 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_4.jpg 4
+
+mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_5.jpg 5
+mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_5.jpg 5
+mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_5.jpg 5
+mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_5.jpg 5
+mpirun -np 5 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_5.jpg 5
+
+mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_6.jpg 6
+mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_6.jpg 6
+mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_6.jpg 6
+mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_6.jpg 6
+mpirun -np 6 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_6.jpg 6
+
+mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_7.jpg 7
+mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_7.jpg 7
+mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_7.jpg 7
+mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_7.jpg 7
+mpirun -np 7 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_7.jpg 7
+
+mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_8.jpg 8
+mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_8.jpg 8
+mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_8.jpg 8
+mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_8.jpg 8
+mpirun -np 8 --hostfile mpi_hosts ./mpi-chroma 4kbg.jpg 4kfg.jpg 4kout_8.jpg 8
+
 
